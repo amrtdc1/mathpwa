8ed4bb92151809f83b7702acb1d2822913617d50
Drew <amrtdc1@users.noreply.github.com>
2025-08-08 22:45:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,35 @@
 const CACHE_NAME = 'math-pwa-cache-v1';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/icon.png'
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cachedRes => {
+      return cachedRes || fetch(event.request).catch(() =>
+        new Response('You are offline and this resource is not cached.')
+      );
     })
   );
 });
